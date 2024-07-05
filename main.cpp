0ea99d353efdbd1b028de65f1d428f2116bff762
viacheslav <slava05mog@mail.ru>
2024-07-06 00:34:18 +0300

    only balanced insert and search

--- main.cpp
+++ main.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include "src/RedBlackTree.h"
+#define NUMBER 1000000
 
-
-std::ostream& operator<<(std::ostream& out, const Color& color)
-{
-    switch (color)
-    {
-        case Color::RED:
-            out << "RED";
-            break;
-        case Color::BLACK:
-            out << "BLACK";
-            break;
-    }
-    return out;
-}
 
 int main()
 {
     RedBlackTree<long, long> rbt;
-    for (long index{0}; index < 10000000; index++)
+    time_t t0 = time(nullptr);
+    for (long index{0}; index < NUMBER; index++)
     {
-        rbt.insertElement(index + 1, 2 * index);
+        rbt.insertElement(rand(), 2 * index);
     }
-    for (long index{0}; index < 10000000; index++)
-    {
-        rbt.searchElement(index + 1);
-    }
+    time_t t1 = time(nullptr);
+    double time_in_seconds = difftime(t1, t0) / NUMBER;
+    std::cout << time_in_seconds << "\n";
+    std::cout << rbt.searchElement(-8786657) << "\n";
+    std::cout << rbt.searchElement(-35345) << "\n";
+    std::cout << rbt.searchElement(20) << "\n";
+    std::cout << rbt.searchElement(78787) << "\n";
+    std::cout << rbt.searchElement(234546) << "\n";
+    std::cout << rbt.searchElement(324348) << "\n";
+//    for (long index = 1; index <= 20; ++index)
+//    {
+//        rbt.insertElement(rand() % 100, 2L);
+//        rbt.printTree();
+//        std::cout << std::endl;
+//    }
+//
+//    rbt.searchElement(93);
+//    rbt.searchElement(36);
     return 0;
 }
--- src/RedBlackTree.h
+++ src/RedBlackTree.h
@@ -33,6 +33,36 @@ private:
     Node<T, K> *root;
     Node<T, K> *nullLeaf;
 
+    void rotate(Node<T, K> *rotatedNode, Node<T, K> *childNode, bool left, Stack<Node<T, K>*> *path)
+    {
+        if (left)
+        {
+            rotatedNode->right = childNode->left;
+            childNode->left = rotatedNode;
+        }
+        else
+        {
+            rotatedNode->left = childNode->right;
+            childNode->right = rotatedNode;
+        }
+        if (rotatedNode == root)
+        {
+            root = childNode;
+        }
+        else
+        {
+            Node<T, K> *parentNode = path->pop();
+            if (parentNode->left == rotatedNode)
+            {
+                parentNode->left = childNode;
+            }
+            else
+            {
+                parentNode->right = childNode;
+            }
+        }
+    }
+
     void leftRotate(Node<T, K> *rotatedNode, Stack<Node<T, K>*> *path)
     {
         if (rotatedNode->right == nullLeaf)
@@ -40,24 +70,7 @@ private:
             return;
         }
         Node<T, K> *rightChild = rotatedNode->right;
-        rotatedNode->right = rightChild->left;
-        if (rotatedNode == root)
-        {
-            root = rightChild;
-        }
-        else
-        {
-            Node<T, K> *fatherNode = path->pop();
-            if (fatherNode->left == rotatedNode)
-            {
-                fatherNode->left = rightChild;
-            }
-            else
-            {
-                fatherNode->right = rightChild;
-            }
-        }
-        rightChild->left = rotatedNode;
+        rotate(rotatedNode, rightChild, true, path);
     }
 
     void rightRotate(Node<T, K> *rotatedNode, Stack<Node<T, K>*> *path)
@@ -67,91 +80,60 @@ private:
             return;
         }
         Node<T, K> *leftChild = rotatedNode->left;
-        rotatedNode->left = leftChild->right;
-        if (rotatedNode == root)
-        {
-            root = leftChild;
-        }
-        else
-        {
-            Node<T, K> *fatherNode = path->pop();
-            if (fatherNode->left == rotatedNode)
-            {
-                fatherNode->left = leftChild;
-            }
-            else
-            {
-                fatherNode->right = leftChild;
-            }
-        }
-        leftChild->right = rotatedNode;
+        rotate(rotatedNode, leftChild, false, path);
     }
 
     void fixInsertion(Node<T, K> *currentNode, Stack<Node<T, K>*> *path)
     {
-        if (path->empty())
-        {
-            currentNode->color = Color::BLACK; //R-B BST property
-            return;
-        }
         while (!(path->empty()) && path->top()->color == Color::RED)
         {
             Node<T, K> *fatherNode = path->pop();
+            if (path->empty())
+            {
+                break;
+            }
             Node<T, K> *grandfatherNode = path->pop();
-            if  (grandfatherNode->left == fatherNode)
+            bool fatherIsLeftChild = (grandfatherNode->left == fatherNode);
+            Node<T, K> *uncleNode = fatherIsLeftChild ? grandfatherNode->right : grandfatherNode->left;
+            if (uncleNode->color == Color::RED) //red uncle case
             {
-                Node<T, K> *uncleNode = grandfatherNode->right;
-                if (uncleNode->color == Color::RED)
+                fatherNode->color = Color::BLACK;
+                uncleNode->color = Color::BLACK;
+                grandfatherNode->color = Color::RED;
+                currentNode = grandfatherNode; //new current node as a grandfather
+                continue;
+            }
+            if (fatherIsLeftChild) // black uncle case (l-r rotate, r rotate)
+            {
+                if (currentNode == fatherNode->right)
                 {
-                    fatherNode->color = Color::BLACK;
-                    uncleNode->color = Color::BLACK;
-                    grandfatherNode->color = Color::RED;
-                    currentNode = grandfatherNode; //repeat for grandfather
+                    path->push(grandfatherNode);
+                    leftRotate(fatherNode, path);
+                    currentNode->color = Color::BLACK;
                 }
                 else
                 {
-                    if (currentNode == fatherNode->right)
-                    {
-                        path->push(grandfatherNode);
-                        leftRotate(fatherNode, path);
-                        currentNode->color = Color::BLACK;
-                    }
-                    else
-                    {
-                        fatherNode->color = Color::BLACK;
-                    }
-                    grandfatherNode->color = Color::RED;
-                    rightRotate(grandfatherNode, path);
-                    break;
-                }
-            }
-            else
-            {
-                Node<T, K> *uncleNode = grandfatherNode->left;
-                if (uncleNode->color == Color::RED)
-                {
                     fatherNode->color = Color::BLACK;
-                    uncleNode->color = Color::BLACK;
-                    grandfatherNode->color = Color::RED;
-                    currentNode = grandfatherNode; //repeat for grandfather
+                }
+                rightRotate(grandfatherNode, path);
+                grandfatherNode->color = Color::RED;
+            }
+            else // black uncle case (r-l rotate, l rotate)
+            {
+                if (currentNode == fatherNode->left)
+                {
+                    path->push(grandfatherNode);
+                    rightRotate(fatherNode, path);
+                    currentNode->color = Color::BLACK;
                 }
                 else
                 {
-                    if (currentNode == fatherNode->left)
-                    {
-                        path->push(grandfatherNode);
-                        rightRotate(fatherNode, path);
-                        currentNode->color = Color::BLACK;
-                    }
-                    else
-                    {
-                        fatherNode->color = Color::BLACK;
-                    }
-                    grandfatherNode->color = Color::RED;
-                    leftRotate(grandfatherNode, path);
-                    break;
+                    fatherNode->color = Color::BLACK;
                 }
+                leftRotate(grandfatherNode, path);
+                grandfatherNode->color = Color::RED;
             }
+            break;
         }
         root->color = Color::BLACK;
     }
@@ -167,22 +149,23 @@ public:
         auto *newNode = new Node<T, K>(key, value, Color::RED, nullLeaf, nullLeaf);
         auto *path = new Stack<Node<T, K>*>(); //safe path to the new node
         Node<T, K> *currentNode = root;
-        while (currentNode != nullLeaf && currentNode->key != key)
+        while (currentNode != nullLeaf)
         {
             path->push(currentNode);
             if (currentNode->key < key)
             {
                 currentNode = currentNode->right;
             }
-            else
+            else if (currentNode->key > key)
             {
                 currentNode = currentNode->left;
             }
-        }
-        if (currentNode != nullLeaf && currentNode->key == key)
-        {
-            delete newNode; //node is already exist
-            return;
+            else
+            { // duplicate case
+                delete newNode;
+                delete path;
+                return;
+            }
         }
         if (path->empty())
         {
@@ -229,152 +212,57 @@ public:
         }
         if (currentNode == nullLeaf)
         {
+            if (currentNode->color == Color::BLACK)
+            {
+                blackDeep++;
+            }
+            else
+            {
+                redDeep++;
+            }
+            std::cout << blackDeep << " " << redDeep << "\n";
             return false;
         }
-        if (currentNode->color == Color::BLACK)
-        {
-            blackDeep++;
-        }
-        else
-        {
-            redDeep++;
-        }
-        std::cout << blackDeep << " " << redDeep << "\n";
         return true;
     }
 
-//    void deleteElement(const T &key)
-//    {
-//        Node<T, K> *currentNode = root;
-//        while (currentNode != nullLeaf && currentNode->key != key)
-//        {
-//            if (currentNode->key < key)
-//            {
-//                currentNode = currentNode->right;
-//            }
-//            else
-//            {
-//                currentNode = currentNode->left;
-//            }
-//        }
-//        if (currentNode == nullLeaf)
-//        {
-//            return;
-//        }
-//        if (currentNode->left == nullLeaf && currentNode->right == nullLeaf)
-//        {
-//            if (currentNode == root)
-//            {
-//                root = nullLeaf;
-//            }
-//            else
-//            {
-//                if (currentNode->parent->left == currentNode)
-//                {
-//                    currentNode->parent->left = nullLeaf;
-//                }
-//                else
-//                {
-//                    currentNode->parent->right = nullLeaf;
-//                }
-//            }
-//            delete currentNode;
-//            return;
-//        }
-//        else if (currentNode->left == nullLeaf || currentNode->right == nullLeaf)
-//        {
-//            Node<T, K> *onlyChild;
-//            if (currentNode->left == nullLeaf)
-//            {
-//                onlyChild = currentNode->right;
-//            }
-//            else
-//            {
-//                onlyChild = currentNode->left;
-//            }
-//            if (currentNode == root)
-//            {
-//                onlyChild->parent = nullLeaf;
-//                root = onlyChild;
-//            }
-//            else
-//            {
-//                onlyChild->parent = currentNode->parent;
-//                if (onlyChild->parent->left == currentNode)
-//                {
-//                    onlyChild->parent->left = onlyChild;
-//                }
-//                else
-//                {
-//                    onlyChild->parent->right = onlyChild;
-//                }
-//            }
-//            delete currentNode;
-//        }
-//        else
-//        {
-//            Node<T, K> *rightSubtreeRoot = currentNode->right;
-//            if (currentNode == root)
-//            {
-//                rightSubtreeRoot->parent = nullLeaf;
-//
-//            }
-//            else
-//            {
-//                rightSubtreeRoot->parent = currentNode->parent;
-//                if (rightSubtreeRoot->parent != nullLeaf)
-//                {
-//                    if (rightSubtreeRoot->parent->left == currentNode)
-//                    {
-//                        rightSubtreeRoot->parent->left = rightSubtreeRoot;
-//                    }
-//                    else
-//                    {
-//                        rightSubtreeRoot->parent->right = rightSubtreeRoot;
-//                    }
-//                }
-//                else
-//                {
-//                    root = rightSubtreeRoot;
-//                }
-//                rightSubtreeRoot->left = currentNode->left;
-//                if (rightSubtreeRoot->left != nullLeaf)
-//                {
-//                    rightSubtreeRoot->left->parent =  rightSubtreeRoot;
-//                }
-//                delete currentNode;
-//            }
-//            else
-//            {
-//                Node<T, K> *leftMostNode = rightSubtreeRoot->left;
-//                while (leftMostNode->left != nullLeaf)
-//                {
-//                    leftMostNode = leftMostNode->left;
-//                }
-//                leftMostNode->parent->left = nullLeaf;
-//                leftMostNode->parent = currentNode->parent;
-//                if (leftMostNode->parent != nullLeaf)
-//                {
-//                    if (leftMostNode->parent->left == currentNode)
-//                    {
-//                        leftMostNode->parent->left = leftMostNode;
-//                    }
-//                    else
-//                    {
-//                        leftMostNode->parent->right = leftMostNode;
-//                    }
-//                }
-//                leftMostNode->left = currentNode->left;
-//                leftMostNode->right = currentNode->right;
-//                currentNode->right->parent = leftMostNode;
-//                if (leftMostNode->left != nullLeaf)
-//                {
-//                    leftMostNode->left->parent = leftMostNode;
-//                }
-//                delete currentNode;
-//            }
-//        }
-//    }
+    void deleteElement(const T &key)
+    {
+        Node<T, K> *deletingNode = root;
+        auto *path = new Stack<Node<T, K>*>(); //safe path to the new node
+        while (deletingNode->key != key)
+        {
+            if (deletingNode == nullLeaf)
+            {
+                delete path;
+                return;
+            }
+            path->push(deletingNode);
+            if (deletingNode->key < key)
+            {
+                deletingNode = deletingNode->right;
+            }
+            else if (deletingNode-> key > key)
+            {
+                deletingNode = deletingNode->left;
+            }
+            else
+            {
+                break;
+            }
+        }
+        Node<T, K> parentNode = path->top();
+        if (deletingNode->left != nullLeaf && deletingNode->right != nullLeaf)
+        {
+            path->push(deletingNode);
+            Node<T, K> nextKeyNode = deletingNode->right;
+            while (nextKeyNode->left != nullLeaf)
+            {
+                path->push(nextKeyNode);
+                nextKeyNode = nextKeyNode->left;
+            }
+        }
+    }
 
     void printHelper(Node<T, K> *node, std::string indent, bool last) const
     {
